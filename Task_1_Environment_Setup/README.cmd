511dd7b63d20feeb327e148eb4e3124350d8f001
Husnain tayyab <husnain143kws@gmail.com>
2026-03-27 19:29:17 +0500

    Add README.cmd for environment setup instructions
    Added a new README.cmd file for environment setup.

--- Task_1_Environment_Setup/README.cmd
+++ Task_1_Environment_Setup/README.cmd
@@ -1,5 +1,5 @@
 # Task 1: Environment Setup
-
+# This is my first contribution
 ## Overview
 This task demonstrates the successful initialization of a Python-based Data Science environment using Google Colab. It verifies the installation and activation of core industry-standard libraries required for data analysis and machine learning.
 
